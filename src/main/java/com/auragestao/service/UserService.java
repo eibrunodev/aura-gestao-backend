c9df5f80ecddd326b75c30768d5fdcf98c7b7582
eibrunodev <eibrunodev@gmail.com>
2026-08-31 13:31:44 -0300

    feat: implementa camada de persistência, regras de negócio e rotas

---
+++ src/main/java/com/auragestao/service/UserService.java
@@ -0,0 +1,19 @@
+package com.auragestao.service;
+
+import com.auragestao.entity.User;
+import com.auragestao.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+}
