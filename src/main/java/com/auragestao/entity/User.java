d4c91f22da6220f75b335da61140f5276c978b32
Wanderson Rafael <w.rafael.s@hotmail.com>
2026-09-02 16:26:20 -0300

    Ajustes Dependencias e notações.

--- src/main/java/com/auragestao/entity/User.java
+++ src/main/java/com/auragestao/entity/User.java
@@ -1,9 +1,11 @@
 package com.auragestao.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -26,44 +28,7 @@ public class User {
     @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
     private LocalDateTime updatedAt;
 
-    public User() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getPhone(){
-        return phone;
-    }
-
-    public void setPhone(String phone){
-        this.phone = phone;
-    }
-
-    public String getCpf(){
-        return cpf;
-    }
-
-    public void setCpf(String cpf){
-        this.cpf = cpf;
-    }
-
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt(){
-        return updatedAt;
-    }
+    @Column(nullable = false, length = 1)
+    private Integer typeUser;
 
 }
